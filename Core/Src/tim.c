306e17acc3f65a7eb89d0a3f751a0c1b9616d33d
junjiemou <3141191264@qq.com>
2024-11-22 20:13:42 +0800

    1

---
+++ Core/Src/tim.c
@@ -0,0 +1,156 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    tim.c
+  * @brief   This file provides code for the configuration
+  *          of the TIM instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "tim.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+TIM_HandleTypeDef htim8;
+DMA_HandleTypeDef hdma_tim8_ch4_trig_com;
+
+/* TIM8 init function */
+void MX_TIM8_Init(void)
+{
+
+  /* USER CODE BEGIN TIM8_Init 0 */
+
+  /* USER CODE END TIM8_Init 0 */
+
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM8_Init 1 */
+
+  /* USER CODE END TIM8_Init 1 */
+  htim8.Instance = TIM8;
+  htim8.Init.Prescaler = 0xffff;
+  htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim8.Init.Period = 65535;
+  htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim8.Init.RepetitionCounter = 0;
+  htim8.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_TRIGGER;
+  sSlaveConfig.InputTrigger = TIM_TS_ETRF;
+  sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_INVERTED;
+  sSlaveConfig.TriggerPrescaler = TIM_TRIGGERPRESCALER_DIV1;
+  sSlaveConfig.TriggerFilter = 0;
+  if (HAL_TIM_SlaveConfigSynchro(&htim8, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim8, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM8_Init 2 */
+
+  /* USER CODE END TIM8_Init 2 */
+
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(tim_baseHandle->Instance==TIM8)
+  {
+  /* USER CODE BEGIN TIM8_MspInit 0 */
+
+  /* USER CODE END TIM8_MspInit 0 */
+    /* TIM8 clock enable */
+    __HAL_RCC_TIM8_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**TIM8 GPIO Configuration
+    PA0-WKUP     ------> TIM8_ETR
+    */
+    GPIO_InitStruct.Pin = PCLK_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF3_TIM8;
+    HAL_GPIO_Init(PCLK_GPIO_Port, &GPIO_InitStruct);
+
+    /* TIM8 DMA Init */
+    /* TIM8_CH4_TRIG_COM Init */
+    hdma_tim8_ch4_trig_com.Instance = DMA2_Stream7;
+    hdma_tim8_ch4_trig_com.Init.Channel = DMA_CHANNEL_7;
+    hdma_tim8_ch4_trig_com.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_tim8_ch4_trig_com.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_tim8_ch4_trig_com.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_tim8_ch4_trig_com.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_tim8_ch4_trig_com.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_tim8_ch4_trig_com.Init.Mode = DMA_NORMAL;
+    hdma_tim8_ch4_trig_com.Init.Priority = DMA_PRIORITY_VERY_HIGH;
+    hdma_tim8_ch4_trig_com.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    if (HAL_DMA_Init(&hdma_tim8_ch4_trig_com) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* Several peripheral DMA handle pointers point to the same DMA handle.
+     Be aware that there is only one stream to perform all the requested DMAs. */
+    __HAL_LINKDMA(tim_baseHandle,hdma[TIM_DMA_ID_CC4],hdma_tim8_ch4_trig_com);
+    __HAL_LINKDMA(tim_baseHandle,hdma[TIM_DMA_ID_TRIGGER],hdma_tim8_ch4_trig_com);
+    __HAL_LINKDMA(tim_baseHandle,hdma[TIM_DMA_ID_COMMUTATION],hdma_tim8_ch4_trig_com);
+
+  /* USER CODE BEGIN TIM8_MspInit 1 */
+
+  /* USER CODE END TIM8_MspInit 1 */
+  }
+}
+
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
+{
+
+  if(tim_baseHandle->Instance==TIM8)
+  {
+  /* USER CODE BEGIN TIM8_MspDeInit 0 */
+
+  /* USER CODE END TIM8_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM8_CLK_DISABLE();
+
+    /**TIM8 GPIO Configuration
+    PA0-WKUP     ------> TIM8_ETR
+    */
+    HAL_GPIO_DeInit(PCLK_GPIO_Port, PCLK_Pin);
+
+    /* TIM8 DMA DeInit */
+    HAL_DMA_DeInit(tim_baseHandle->hdma[TIM_DMA_ID_CC4]);
+    HAL_DMA_DeInit(tim_baseHandle->hdma[TIM_DMA_ID_TRIGGER]);
+    HAL_DMA_DeInit(tim_baseHandle->hdma[TIM_DMA_ID_COMMUTATION]);
+  /* USER CODE BEGIN TIM8_MspDeInit 1 */
+
+  /* USER CODE END TIM8_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
---
+++ Core/modules/SEEKFREE_7725.c
@@ -0,0 +1,277 @@
+/*********************************************************************************************************************
+ * COPYRIGHT NOTICE
+ * Copyright (c) 2018,逐飞科技
+ * All rights reserved.
+ * 技术讨论QQ群：一群：179029047(已满)  二群：244861897(已满)  三群：824575535
+ *
+ * 以下所有内容版权均属逐飞科技所有，未经允许不得用于商业用途，
+ * 欢迎各位使用并传播本程序，修改内容时必须保留逐飞科技的版权声明。
+ *
+ * @file       		小钻风(二值化摄像头)
+ * @company	   		成都逐飞科技有限公司
+ * @author     		逐飞科技(QQ3184284598)
+ * @version    		查看doc内version文件 版本说明
+ * @Software 		MDK 5.24
+ * @Taobao   		https://seekfree.taobao.com/
+ * @date       		2020-01-03
+ * @note		
+					OV7725接线定义：
+					------------------------------------
+					查看2-HAL OV7725 UART PC.ioc文件
+					------------------------------------
+ ********************************************************************************************************************/
+
+
+
+#include "SEEKFREE_7725.h"
+#include "SEEKFREE_IIC.h"
+#include "tim.h"
+#include "stm32f4xx_it.h"
+#include "usart.h"
+
+uint8_t image_bin[OV7725_SIZE];                                   //定义存储接收图像的数组
+uint8_t ov7725_finish_flag = 0;
+uint8_t OV7725_IDCode = 0;
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头内部寄存器初始化(内部使用，用户无需调用)
+//  @param      NULL
+//  @return     uint8_t			返回0则出错，返回1则成功
+//  @since      v1.0
+//  Sample usage:
+//-------------------------------------------------------------------------------------------------------------------
+uint8_t ov7725_reg_init(void)
+{
+    simiic_write_reg ( OV7725_DEV_ADD, OV7725_COM7, 0x80 );	//复位摄像头
+    HAL_Delay(50);
+
+    while( OV7725_IDCode != OV7725_ID )
+	{
+		OV7725_IDCode = simiic_read_reg( OV7725_DEV_ADD, OV7725_VER,SCCB);
+		HAL_Delay(1);
+		//校验摄像头ID号
+	}
+
+    if(OV7725_IDCode == OV7725_ID)
+    {
+        //ID号确认无误   然后配置寄存器
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_COM4, 0xC1);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_CLKRC, 0x01);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_COM2, 0x03);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_COM3, 0xD0);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_COM7, 0x40);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_HSTART, 0x3F);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_HSIZE, 0x50);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_VSTRT, 0x03);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_VSIZE, 0x78);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_HREF, 0x00);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_SCAL0, 0x0A);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_AWB_Ctrl0, 0xE0);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_DSPAuto, 0xff);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_DSP_Ctrl2, 0x0C);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_DSP_Ctrl3, 0x00);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_DSP_Ctrl4, 0x00);
+
+
+        if(OV7725_W == 80)              simiic_write_reg(OV7725_DEV_ADD, OV7725_HOutSize, 0x14);
+        else if(OV7725_W == 160)        simiic_write_reg(OV7725_DEV_ADD, OV7725_HOutSize, 0x28);
+        else if(OV7725_W == 240)        simiic_write_reg(OV7725_DEV_ADD, OV7725_HOutSize, 0x3c);
+        else if(OV7725_W == 320)        simiic_write_reg(OV7725_DEV_ADD, OV7725_HOutSize, 0x50);
+
+        if(OV7725_H == 60)              simiic_write_reg(OV7725_DEV_ADD, OV7725_VOutSize, 0x1E);
+        else if(OV7725_H == 120)        simiic_write_reg(OV7725_DEV_ADD, OV7725_VOutSize, 0x3c);
+        else if(OV7725_H == 180)        simiic_write_reg(OV7725_DEV_ADD, OV7725_VOutSize, 0x5a);
+        else if(OV7725_H == 240)        simiic_write_reg(OV7725_DEV_ADD, OV7725_VOutSize, 0x78);
+
+
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_REG28, 0x01);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_EXHCH, 0x10);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_EXHCL, 0x1F);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM1, 0x0c);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM2, 0x16);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM3, 0x2a);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM4, 0x4e);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM5, 0x61);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM6, 0x6f);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM7, 0x7b);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM8, 0x86);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM9, 0x8e);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM10, 0x97);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM11, 0xa4);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM12, 0xaf);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM13, 0xc5);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM14, 0xd7);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_GAM15, 0xe8);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_SLOP, 0x20);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_LC_RADI, 0x00);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_LC_COEF, 0x13);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_LC_XC, 0x08);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_LC_COEFB, 0x14);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_LC_COEFR, 0x17);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_LC_CTR, 0x05);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_BDBase, 0x99);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_BDMStep, 0x03);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_SDE, 0x04);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_BRIGHT, 0x00);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_CNST, 0x40);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_SIGN, 0x06);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_UVADJ0, 0x11);
+        simiic_write_reg(OV7725_DEV_ADD, OV7725_UVADJ1, 0x02);
+        return 1;
+    }
+    else        return 0;//错误
+}
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头场中断回调函数
+//  @param      NULL
+//  @return     void
+//  @since      v1.0
+//  Sample usage:					该函数在stm32f1xx_it.c文件中的EXTI0_IRQHandler函数中进行回调
+//-------------------------------------------------------------------------------------------------------------------
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    //使能TIM触发DMA链路。
+    __HAL_TIM_ENABLE_DMA(&htim8, TIM_DMA_TRIGGER);
+     //重置DMA
+    HAL_DMA_Start_IT(&hdma_tim8_ch4_trig_com, (uint32_t)&GPIOE->IDR, (uint32_t)image_bin, OV7725_SIZE);
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头DMA完成中断
+//  @param      NULL
+//  @return     void
+//  @since      v1.0
+//  Sample usage:					//该函数已在SEEKFREE_7725.c中的ov7725_port_init函数中注册。
+                                    //该函数在stm32f1xx_it.c文件中的DMA1_Channel4_IRQHandler函数中进行回调
+//-------------------------------------------------------------------------------------------------------------------
+void DMA_transfer_complete_callback(DMA_HandleTypeDef * _hdma)
+{
+    //图像传输完成标志位置1
+    ov7725_finish_flag = 1;			
+    //失能TIM触发DMA链路。
+    __HAL_TIM_DISABLE_DMA(&htim8, TIM_DMA_TRIGGER);
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头采集程序初始化(内部使用，用户无需调用)
+//  @param      NULL
+//  @return     void
+//  @since      v1.0
+//  Sample usage:
+//-------------------------------------------------------------------------------------------------------------------
+void ov7725_port_init(void)
+{
+    //dma传输完成中断回调函数注册
+    while(HAL_OK != HAL_DMA_RegisterCallback(&hdma_tim8_ch4_trig_com,HAL_DMA_XFER_CPLT_CB_ID,DMA_transfer_complete_callback))
+    {
+        HAL_DMA_Abort_IT(&hdma_tim8_ch4_trig_com);
+    }
+    
+    //打开DMA中断
+    HAL_DMA_Start_IT(&hdma_tim8_ch4_trig_com, (uint32_t)&GPIOF->IDR, (uint32_t)image_bin, OV7725_SIZE);
+    
+    //使能TIM触发DMA链路
+    __HAL_TIM_ENABLE_DMA(&htim8, TIM_DMA_TRIGGER);
+}
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头初始化(调用之后设置好相关中断函数即可采集图像)
+//  @param      NULL
+//  @return     0
+//  @since      v1.0
+//  Sample usage:
+//-------------------------------------------------------------------------------------------------------------------
+uint8_t ov7725_init(void)
+{
+    ov7725_reg_init();          //摄像头寄存器配置
+    ov7725_port_init();         //TIM和DMA使能
+    return 0;
+}
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      串口输出8位数据
+//  @param      USARTx			串口号
+//  @param      Data			8位数据
+//  @return     void
+//  @since      v1.0
+//  Sample usage:				调用该函数前请先初始化串口
+//-------------------------------------------------------------------------------------------------------------------
+void put_char(UART_HandleTypeDef* USARTx, uint8_t Data)
+{
+    while((USARTx->Instance->SR & 0X40)==0);
+    USARTx->Instance->DR = Data;
+}
+
+
+        
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头未解压图像发送至上位机查看图像
+//  @param      *imgaddr			压缩图像数据地址
+//  @param      *imgsize			图像大小(直接填写OV7725_SIZE)
+//  @return     void
+//  @since      v1.0
+//  Sample usage:					调用该函数前请先初始化串口
+//-------------------------------------------------------------------------------------------------------------------
+void seekfree_sendimg_7725(void *imgaddr, uint32_t imgsize)
+{
+//建议不要使用HAL库自带的串口发送函数，由于HAL库自带的串口发送函数多余的操作较多，可能导致串口发送数据不完整。
+//建议不要使用HAL库自带的串口发送函数，由于HAL库自带的串口发送函数多余的操作较多，可能导致串口发送数据不完整。
+//建议不要使用HAL库自带的串口发送函数，由于HAL库自带的串口发送函数多余的操作较多，可能导致串口发送数据不完整。
+
+//使用HAL库串口发送函数发送图像数据。
+//    uint8_t send_buff[4] = {0x00,0xFF,0x01,0x01};
+//    HAL_UART_Transmit(&huart1,send_buff,4,0xffff);
+//    HAL_UART_Transmit(&huart1,imgaddr,imgsize/2,10000);
+  
+//使用寄存器发送图像数据。
+	uint32_t i;
+	put_char(&huart1,0x00);
+    put_char(&huart1,0xff);
+    put_char(&huart1,0x01);
+    put_char(&huart1,0x01);
+    for(i=0; i<(imgsize); i++)
+    {
+        put_char(&huart1,((uint8_t *)(imgaddr))[i]);
+    }
+}
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//  @brief      小钻风摄像头数据解压函数
+//  @param      *data1				源地址
+//  @param      *data2				目的地址
+//  @return     void
+//  @since      v1.0
+//  Sample usage:					Image_Decompression(da1,dat2[0]);//将一维数组dat1的内容解压到二维数组dat2里.
+//-------------------------------------------------------------------------------------------------------------------
+void Image_Decompression(uint8_t *data1,uint8_t *data2)
+{
+    uint8_t  temp[2] = {0,255};
+    uint16_t lenth = OV7725_SIZE;
+    uint8_t  i = 8;
+    while(lenth--)
+    {
+        i = 8;
+        while(i--)
+        {
+            *data2++ = temp[(*data1 >> i) & 0x01];
+        }
+        data1++;
+    }
+}
+
+
